6c8328c52b4d933adc5de5c5536a20301d70a067
smritimishrakiet <69758887+smritimishrakiet@users.noreply.github.com>
2020-08-26 17:42:43 +0530

    Add files via upload

--- experiment/simulation/js/lab.js
+++ experiment/simulation/js/lab.js
@@ -10,20 +10,15 @@ var material_conductivity = 0.48;
 var heat_input = voltage * current;
 var temp_diff = outer_temp - ambient_temp;
 
-var heat_transfer_coeff = heat_input/(surface_area * temp_diff);
+var heat_transfer_coeff = ( heat_input/(surface_area * temp_diff)).toFixed(3);
     //return heat_transfer_coeff;
 document.getElementById("heat_transfer_coeff").innerHTML=heat_transfer_coeff;
-var radius = material_conductivity/heat_transfer_coeff;
+var radius = (material_conductivity/heat_transfer_coeff).toFixed(3);
 document.getElementById("radius").innerHTML=radius;
-
-    //var voltage = document.getElementById("user_voltage").value;
-    document.getElementById("click_voltage").value = voltage;
-    //var current = document.getElementById("user_current").value;
-    document.getElementById("click_current").value = current;
-    //var inner_temp = document.getElementById("user_inner_temp").value;
-    document.getElementById("click_temp1").value = inner_temp;
-    //var outer_temp = document.getElementById("user_outer_temp").value;
-    document.getElementById("click_temp2").value = outer_temp;
-    //var ambient_temp = document.getElementById("user_ambient_temp").value;
-    document.getElementById("click_temp3").value = ambient_temp;
+document.getElementById("amp").setAttribute("data-value", current);
+document.getElementById("vol").setAttribute("data-value", voltage);
+document.getElementById("temp1").setAttribute("data-value", inner_temp);
+document.getElementById("temp2").setAttribute("data-value", outer_temp);
+document.getElementById("temp3").setAttribute("data-value", ambient_temp); 
     }
+  
--- experiment/simulation/js/vlab.js
+++ experiment/simulation/js/vlab.js
@@ -6,3 +6,44 @@ $(document).ready(function(){
     });
 });
 
+function hideSVG() {
+    var style = document.getElementById("myRect").style.display;
+    if(style === "block")
+    {
+      document.getElementById("myRect").style.display = "none";
+      document.getElementById("myBtn").disabled = true;
+      document.getElementById("onoff").value="Power ON";
+      document.getElementById("amp").setAttribute("data-value", 0);
+      document.getElementById("vol").setAttribute("data-value", 0);
+      document.getElementById("temp1").setAttribute("data-value", 0);
+      document.getElementById("temp2").setAttribute("data-value", 0);
+      document.getElementById("temp3").setAttribute("data-value", 0); 
+      
+    }
+      
+    else
+    {
+      document.getElementById("myRect").style.display = "block";
+      document.getElementById("myBtn").disabled = false;
+      document.getElementById("onoff").value="Power OFF";
+    }
+
+      
+    var dfgf = document.getElementById("switch").style.display;
+    if(dfgf === "block")
+      document.getElementById("switch").style.display = "none";
+    else
+      document.getElementById("switch").style.display = "block";
+    //or to hide the all svg
+    //document.getElementById("mySvg").style.display = "none";
+  }
+
+    $(document).ready(function(){
+        $("#myModal").modal('show');
+    });
+      
+   
+   
+
+
+  
